7c71565e18a323c84de0b2384961e9415c1c3863
JoelHuancaZevallos <u20201b914@upc.edu.pe>
2025-10-10 11:52:17 -0500

    feat: add Vehicle and VehicleDto classes for vehicle management

---
+++ src/app/features/listings/models/vehicle.model.ts
@@ -0,0 +1,14 @@
+export class Vehicle {
+  public ownerName?: string;
+
+  constructor(
+    public id: number,
+    public brand: string,
+    public model: string,
+    public year: number,
+    public pricePerDay: number,
+    public status: 'available' | 'rented',
+    public imageUrl: string,
+    public ownerId: number
+  ) {}
+}
